a854921b01141a3f3f9ce5202da6dab544012a64
Nitish <tnitish654@gmail.com>
2025-11-11 18:30:45 +0800

    Initial Commit

---
+++ smpl widgets/smpl_widgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  smpl_widgetsApp.swift
+//  smpl widgets
+//
+//  Created by Nitish on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct smpl_widgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
